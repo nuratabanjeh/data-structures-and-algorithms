f75a478161ec378f4b61cade2efb85523d2c6b16
nuratabanjeh <nuratabanjeh96@gmail.com>
2021-08-01 22:51:17 +0300

    codech

--- python/code_challenges/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list.py
@@ -9,9 +9,7 @@ class LinkedList:
         self.head = None
 
     def insert(self, value='null'):
-        """
-        Adds a node of a value to the head of LL
-        """
+      
         try:
           node = Node(value)
           if not self.head:
@@ -20,144 +18,41 @@ class LinkedList:
               current = self.head
               self.head= node
               self.head.next=current
-        except Exception as e:
-          raise Exception(f"Something's Going Wrong : {e}")
+        except Exception as error:
+          raise Exception(f"Something Wrong : {error}")
 
 
-    def append(self, value='null'):
-        """
-        Adds a node of a value to the end of LL
-        """
+
+    def includes(self,n):
+        
         try:
-          node = Node(value)
-          if not self.head:
-              self.head = node
-
-          else:
-              current = self.head
-              while current.next != None:
-                  current = current.next
-              current.next = node
-        except Exception as e:
-          raise Exception(f"Something's Going Wrong : {e}")
-
-    def includes(self,num):
-        """
-        Return T/F if value is in the linked list or not
-        """
-        try:
-          x=False
+          result=False
 
           current = self.head
           while current:
-              if current.value==num:
+              if current.value==n:
 
-                  x=True
+                  result=True
                   break
               current=current.next
-          return x
-        except Exception as e:
-          raise Exception(f"Something's Going Wrong : {e}")
+          return result
+        except Exception as error:
+          raise Exception(f"Something Wrong : {error}")
 
-    def insertBefore(self ,value, newVal):
-        """
-        add a new node with the given newValue immediately before the first value node
-        """
-        current = self.head
-        if current.value==value:
-            self.insert(newVal)
-        else:
-          while current:
-
-             if current.next.value==value :
-                nextvalue=current.next
-                current.next=Node(newVal)
-                current.next.next=nextvalue
-                break
-             current=current.next
-
-
-    def insertAfter(self, value, newVal):
-        """
-        add a new node with the given newValue immediately after the first value node
-        """
-        current = self.head
-        while current:
-            if current.value==value :
-                nextvalue=current.next
-                current.next=Node(newVal)
-                current.next.next=nextvalue
-                break
-            current=current.next
-
-    def kthFromEnd(self,k):
-        if type(k)!=type(5):
-            return 'Please enter number input'
-        if k<0:
-            return "Can't enter negative input"
-        valuelist=[]
-        current = self.head
-        while current:
-            valuelist+=[current.value]
-
-            current=current.next
-        if k==0:
-            return valuelist[-1]
-        else:
-            if k>=len(valuelist):
-                return 'Exception'
-            return valuelist[(k*-1)-1]
-
-
-
+   
 
 
     def __str__(self):
-        """
-        Loop over all nodes
-        print all values in one line
-        ( a ) -> ( b ) -> ( c ) -> None
-        """
-
         output = ""
         current = self.head
         while current:
             value = current.value
             if current.next is None:
-                output += f"( {value} ) -> None"
+                output += f"( {value} ) => None"
                 break
-            output = output + f"( {value} ) -> "
+            output = output + f"( {value} ) => "
             current=current.next
         return output
-
-
-def palindrome(ll):
-  valuelist=[]
-  current = ll.head
-  while current:
-    valuelist+=[current.value]
-    current=current.next
-
-  for i in range(len(valuelist)//2):
-    if valuelist[i]!=valuelist[-1*i-1]:
-      return False
-  return True
-
-def reverse(ll):
-  valuelist=[]
-  x='head->'
-  current = ll.head
-  while current:
-    valuelist+=[current.value]
-    current=current.next
-  print(valuelist)
-  for i in range(len(valuelist)):
-      if i==len(valuelist)-1:
-          x+=f'({valuelist[-1*i-1]})'
-      else:
-          x+=f'({valuelist[-1*i-1]})->'
-
-  return x
 
 
 
@@ -171,15 +66,20 @@ def reverse(ll):
 
 if __name__ == "__main__":
 
-    ll = LinkedList()
-    ll.append(1)
-    ll.append(2)
-    ll.append(3)
-    ll.append(2)
-    ll.append(1)
+    lList = LinkedList()
+    print(lList)
+    lList.insert(5)
+    print(lList)
+    lList.insert(6)
+    print(lList)
+    print(lList.includes(6))
+    print(lList)
+    lList.includes(4)
+    print(lList.includes(4))
+    print(lList)
+    # lList.append(2)
+    # lList.append(3)
+    # lList.append(2)
+    # lList.append(1)
 
-    # ll.append(1)
-    # ll.append(4)
-    print(ll.__str__())
-    print(palindrome(ll))
-    # print(reverse(ll))
+ 
