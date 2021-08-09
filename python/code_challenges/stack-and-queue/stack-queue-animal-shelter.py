5935bacaa7e2e4f8ffa74f95c623d2292ff3d9f4
nuratabanjeh <nuratabanjeh96@gmail.com>
2021-08-09 23:24:36 +0300

    pre

---
+++ python/code_challenges/stack-and-queue/stack-queue-animal-shelter.py
@@ -0,0 +1,19 @@
+class AnimalShelter:
+    def __init__(self):
+        pass
+
+
+    def enqueue():
+        pass
+
+
+    def dequeue():
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    animal=AnimalShelter()
+    
