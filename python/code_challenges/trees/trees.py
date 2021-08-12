30378c98996e36e6459539757304fdf7b276863f
nuratabanjeh <nuratabanjeh96@gmail.com>
2021-08-12 14:37:53 +0300

    initial

---
+++ python/code_challenges/trees/trees.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def pre_order(self):
+        pass
+
+    def in_order(self):
+        pass
+
+    def post_order(self):
+        pass
+
+class BinarySearchTree(BinaryTree):
+    def add(self,value):
+        pass
+    
+    def contains(self,value):
+         pass
