39234f9b80d0711f163911ae5f71cb2370a407d4
nuratabanjeh <nuratabanjeh96@gmail.com>
2021-08-14 04:33:25 +0300

    not finish

--- python/code_challenges/trees/trees.py
+++ python/code_challenges/trees/trees.py
@@ -8,18 +8,100 @@ class BinaryTree:
     def __init__(self):
         self.root = None
 
+## i useded recursive function(call back function)
+
     def pre_order(self):
-        pass
+        treeList=[]
+        def _pre_order(root):
+            if not root:
+                return
+            treeList.append(root.value)
+            if root.left:
+                _pre_order(root.left)
+            if root.right:
+                _pre_order(root.right)
+        
+        _pre_order(self.root)
+        return treeList
+            
 
     def in_order(self):
-        pass
+        treeList=[]
+        def _in_order(root):
+            if not root:
+                return
+
+            if root.left:
+                _in_order(root.left)
+            treeList.append(root.value)
+            if root.right:
+                _in_order(root.right)
+        _in_order(self.root)
+        return treeList
+
 
     def post_order(self):
-        pass
+        treeList=[]
+        def _post_order(root):
+            if not root:
+                return
+            if root.left:
+                _post_order(root.left)
+            if root.right:
+                _post_order(root.right)
+
+            treeList.append(root.value)
+        _post_order(self.root)
+        return treeList
 
 class BinarySearchTree(BinaryTree):
     def add(self,value):
-        pass
+        addingNode = Node(value)
+        currentNode = self.root
+        if addingNode.value == currentNode:
+            return "the value must be unique"
+        if not self.root:
+            self.root = addingNode
+        else:
+            def _add(addingNode , currentNode ):
+                while currentNode.value != addingNode.value:
+                    if addingNode.value < currentNode.value:
+                        if not currentNode.left:
+                            currentNode.left = addingNode
+                            return
+                        else:
+                            currentNode == currentNode.left 
+                            _add(addingNode,currentNode)
+                    else:
+                        if not currentNode.right :
+                            currentNode.right = addingNode
+                            return
+                        else:
+                            currentNode = currentNode.right
+                            _add(addingNode,currentNode)
+
+
+            _add(addingNode,currentNode)
+
+
+
+
+        
+
     
     def contains(self,value):
          pass
+
+if __name__ == "__main__":
+    tree = BinarySearchTree()
+    tree.add(1)
+    tree.add(2)
+    tree.add(3)
+    tree.add(14)
+    tree.add(4)
+    print(tree.root.value,'************')
+    print(tree.pre_order())
+    print(tree.in_order())
+    print(tree.post_order())
+
+    ## [2,1,4,3]
