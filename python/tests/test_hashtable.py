cd0eead564ffa2f8b9cdd63ef5d858dfb28e2535
nuratabanjeh <nuratabanjeh96@gmail.com>
2021-09-04 22:21:09 +0300

    codech30

--- python/tests/test_hashtable.py
+++ python/tests/test_hashtable.py
@@ -1,11 +1,5 @@
-
-
-
 class Node:
-  ''''
-  THis the class is responsiple to create the Nodes 
-  '''
-  def __init__(self, value=""):
+  def __init__(self, value):
     self.value = value
     self.next = None
 
@@ -14,62 +8,50 @@ class Node:
 
   def __str__(self):
     return str(self.value)
+  def insert(self, value):
+    node = Node(value)
+    if self.head:
+        node.next = self.head
+    self.head = node   
 
 
-class LinkedList():
-  
-  '''
-  This Class Responsable For Creating LinkdeList 
-  '''
+class LinkedList:
   def __init__(self):
     self.head = None
 
-  def insert(self, value):
-    node = Node(value)
-    # print(node)
-    if self.head:
-      node.next = self.head
-
-    self.head = node
+  
 
   def includes(self,vlaue):
-     current=self.head
-     while current :
-        #  print(current.value)
-         if vlaue ==current.value[0]:
-            return True
-         current=current.next
-     return False
+      current=self.head
+      while current :
+          if vlaue ==current.value[0]:
+              return True
+          current=current.next
+      return False
 
   def append(self,value):
-    new_node=Node(value)
+      new_node=Node(value)
 
-    if self.head == None:
-      self.head = new_node
-    else:
-      current=self.head
-      while current.next:
-        current=current.next
-      current.next=new_node
+      if self.head == None:
+          self.head = new_node
+      else:
+          current=self.head
+          while current.next:
+              current=current.next
+          current.next=new_node
 
-
-class HashTable :
-
+class Hashtable:
   def __init__(self, size = 1024):
+      self.size = size
+      self._buckets = [None]*size 
     
-    self.size = size
-    self._buckets = [None] *self.size
-
-
-
-  def hash(self,key:str)->int:
+  def hash(self,key):
     value=0
     for x in key:
       value += ord(x)
     index = (value * 7) % self.size   
     return index
     
-
   def add(self,key,value):
     '''add a value to the hashtable by its key 
     parameters:
@@ -79,12 +61,6 @@ class HashTable :
     return: nothing
 
     '''
-    # index =self.hash(key)
-    # if not self._buckets[index]:
-    #   self._buckets[index]=[[key,value]]
-    # else:
-    #   self._buckets[index].append([key,value])
-
     index = self.hash(key) 
 
     if not self._buckets[index]:
@@ -92,18 +68,6 @@ class HashTable :
       
 
     self._buckets[index].append([key,value])
-
-
-
-  def find(self,key):
-    """this function will search in the hashtable about the key and return the value
-    parameters:
-      key: a string
-    return: the value 
-      """
-    index = self.hash[key]
-    return index
-    
 
   def contains(self,key):
     """this function will check if the there is a value for the key 
@@ -116,12 +80,43 @@ class HashTable :
       return self._buckets[index].includes(key)
     else:
       return False
+
+  def get(self,key):
+
+      index = self.hash(key)
+      if self._buckets[index] == None:
+          return None
+      else:
+          current=self._buckets[index].head
+          while current:
+              if current.value[0] == key:
+                  return current.value[1]
+              current = current.next
     
 
 ######Test####
 
 
+
 def test_add():
-  test=HashTable()
-  test.add('ahmad',555)
-  assert test.contains('ahmad')==True 
+    test=Hashtable()
+    test.add('Nura',25)
+    assert test.contains('Nura')==True
+
+def test_null_for_key_not_exist():
+    test = Hashtable()
+    test=test.get('Nura')
+    expected=None
+    assert test==expected
+
+def test_collision():
+    test = Hashtable()
+    test.add('Nura', 96)
+    test.add('mhmoud', 90)
+    assert test.get('Nura') == 96
+
+def test_retrieve_value_from_hashtable():
+    test = Hashtable()
+    test.add('Nura', 96)
+    test.add('Mhmoud', 93)
+    assert (test.get('Nura') == 96 and test.get('Mhmoud')== 93)
