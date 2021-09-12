3c07097d977d568fb68cca74d67174ccb199cc78
nuratabanjeh <nuratabanjeh96@gmail.com>
2021-09-13 00:48:26 +0300

    done

--- python/tests/test_graph_breadth_first.py
+++ python/tests/test_graph_breadth_first.py
@@ -18,8 +18,8 @@ class Queue():
 class Vertex:
     def __init__(self, value=None):
         self.value = value
-    def __str__(self):
-        return f'Vertix > {self.value}'
+    # def __str__(self):
+    #     return f'Vertix > {self.value}'
 
 class Edge:
     def __init__(self, vertix , weight):
@@ -78,21 +78,16 @@ class Graph:
                     breadth.enqueue(edge.vertix)
 
         return all_nodes
+#############TEST###########
 
-
-
-
-
-if __name__ == '__main__':
+def test_breadth_first():
     graph = Graph()
-
     pandora= graph.add_vertex('pandora')
     arendelle= graph.add_vertex('arendelle')
     metroville= graph.add_vertex('metroville')
     narina= graph.add_vertex('narina')
     naboo= graph.add_vertex('naboo')
     manstropolis= graph.add_vertex('manstropolis')
-
     graph.add_edge(pandora,arendelle)
     graph.add_edge(arendelle,pandora)
     graph.add_edge(arendelle,metroville)
@@ -109,5 +104,44 @@ if __name__ == '__main__':
     graph.add_edge(manstropolis,naboo)
     graph.add_edge(manstropolis,arendelle)
     graph.add_edge(manstropolis,metroville)
+    actual=graph.breadth_first(pandora)
+    expected=[pandora,arendelle,metroville,manstropolis,naboo,narina]
+    assert actual==expected
 
-    print(graph.breadth_first(pandora))
+
+
+def test_edge_cases():
+    graph = Graph()
+    pandora=Vertex('pandora')
+    actual=graph.breadth_first(pandora)
+    expected='Exception'
+    assert actual==expected
+
+
+def test_expected_failure():
+    graph = Graph()
+    pandora= graph.add_vertex('pandora')
+    arendelle= graph.add_vertex('arendelle')
+    metroville= graph.add_vertex('metroville')
+    narina= graph.add_vertex('narina')
+    naboo= graph.add_vertex('naboo')
+    manstropolis= graph.add_vertex('manstropolis')
+    graph.add_edge(pandora,arendelle)
+    graph.add_edge(arendelle,pandora)
+    graph.add_edge(arendelle,metroville)
+    graph.add_edge(arendelle,metroville)
+    graph.add_edge(metroville,arendelle)
+    graph.add_edge(metroville,manstropolis)
+    graph.add_edge(metroville,naboo)
+    graph.add_edge(metroville,narina)
+    graph.add_edge(narina,metroville)
+    graph.add_edge(narina,naboo)
+    graph.add_edge(naboo,narina)
+    graph.add_edge(naboo,metroville)
+    graph.add_edge(naboo,manstropolis)
+    graph.add_edge(manstropolis,naboo)
+    graph.add_edge(manstropolis,arendelle)
+    graph.add_edge(manstropolis,metroville)
+    actual=graph.breadth_first(pandora)
+    expected=[pandora,arendelle,metroville,manstropolis,narina,naboo]
+    assert actual!=expected
